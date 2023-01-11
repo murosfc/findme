b7f0979d87f050aafc9927476d9b7d52de4b2372
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-01-10 22:10:13 -0300

    Add: cores padrão e menu

---
+++ lib/MainDrawer.dart
@@ -0,0 +1,74 @@
+import 'package:findme/colors/VisualIdColors.dart';
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black87,
+      child: Column(
+        children: [
+          Container(
+            color: VisualIdColors.colorBlue(),
+            child: Padding(
+              padding: EdgeInsets.only(top: 50.0, left: 20.0),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Menu",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white70,
+                    ),
+                    //textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.add,
+              color: Colors.white70,
+            ),
+            title: Text("Adicionar contato",
+                style: TextStyle(color: Colors.white70)),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.star_border,
+              color: Colors.white70,
+            ),
+            title: Text("Gerenciar favoritos",
+                style: TextStyle(color: Colors.white70)),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.lock,
+              color: Colors.white70,
+            ),
+            title: Text("Permissões", style: TextStyle(color: Colors.white70)),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(
+              Icons.settings,
+              color: Colors.white70,
+            ),
+            title:
+                Text("Configurações", style: TextStyle(color: Colors.white70)),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:findme/MainDrawer.dart';
+import 'package:findme/colors/VisualIdColors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,44 +8,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {    
-    var themeData = ThemeData(
-        brightness: Brightness.dark,        
-      );
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Find'nMe",     
-      theme: ThemeData(  
-        brightness: Brightness.light,        
+      theme: ThemeData(
+        primarySwatch: VisualIdColors.colorBlue(),
+        scaffoldBackgroundColor: Color(0xFF121212),
       ),
-      darkTheme: themeData,
-      themeMode: ThemeMode.dark, 
-      /* ThemeMode.system to follow system theme, 
-         ThemeMode.light for light theme, 
-         ThemeMode.dark for dark theme
-      */
-      debugShowCheckedModeBanner: false,       
       home: const MyHomePage(title: "Find'nMe"),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -64,36 +44,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        centerTitle: true,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              //procurar
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: MainDrawer(),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -106,6 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
