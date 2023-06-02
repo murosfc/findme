0780f14029214e9984aea5836dd039c959dbdc6d
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-06-01 22:19:13 -0300

    volta da classe com objeto da biblioteca

--- lib/pages/ARScreenSquare.dart
+++ lib/pages/ARScreenSquare.dart
@@ -1,10 +1,9 @@
-import 'dart:async';
-
-import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:localization/localization.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:vector_math/vector_math_64.dart' as vector_math;
+import 'package:localization/localization.dart';
+
+import '../colors/VisualIdColors.dart';
 
 class ARScreenSquare extends StatefulWidget {
   final String userName;
@@ -17,16 +16,12 @@ class ARScreenSquare extends StatefulWidget {
 
 class _ARScreenSquareState extends State<ARScreenSquare> {
   late ArCoreController arCoreController;
+  bool isObjectPlaced = false;
 
   @override
   void dispose() {
     arCoreController.dispose();
     super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
   }
 
   void _onARViewCreated(ArCoreController controller) {
@@ -35,37 +30,49 @@ class _ARScreenSquareState extends State<ARScreenSquare> {
   }
 
   void _addARObject() async {
-    final square = ArCoreCube(
-      materials: [ArCoreMaterial(color: Colors.red)],
-      size: vector_math.Vector3(0.2, 0.2, 0.2),
+    final fixedVertex = vector_math.Vector3(0.0, 0.0, -1.0);
+    final rotatingNode = ArCoreRotatingNode(
+      shape: ArCoreCube(
+        materials: [
+          ArCoreMaterial(
+            color: VisualIdColors.colorBlue(),
+            reflectance: 0.5,
+          ),
+        ],
+        size: vector_math.Vector3(0.2, 0.01, 0.2),
+      ),
+      position: fixedVertex,
+      rotation: vector_math.Vector4(0.0, 1.0, 0.0, 0.0),
     );
-
-    final squareNode = ArCoreNode(
-      shape: square,
-      position: vector_math.Vector3(0, 0, -1), // Set the position of the square
-      name: 'square',
-    );
-
-    arCoreController.addArCoreNodeWithAnchor(squareNode);
+    arCoreController.addArCoreNodeWithAnchor(rotatingNode);
+    setState(() {
+      isObjectPlaced = true;
+    });
   }
 
-  void _updateARObjectPosition() {
-    // TODO: Implement code to update the AR object's position
-    // based on your specific requirements.
+  void _removeARObject() {
+    arCoreController.removeNode(nodeName: 'rotating_cube');
+    setState(() {
+      isObjectPlaced = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     var finding = 'finding'.i18n();
-    var nome = "$finding ${widget.userName}";
+    var name = "$finding ${widget.userName}";
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(nome),
+        title: Text(name),
       ),
       body: ArCoreView(
         onArCoreViewCreated: _onARViewCreated,
         enableTapRecognizer: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: isObjectPlaced ? _removeARObject : _addARObject,
+        child: Icon(isObjectPlaced ? Icons.remove : Icons.add),
       ),
     );
   }
