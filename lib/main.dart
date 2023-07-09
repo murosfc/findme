d2e6bb42f7cec5d83d63f7187bcd4683f57d2618
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-07-09 20:16:56 -0300

    tentativa de direcionar no click de notificação

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:findme/pages/Location/LocationConfirmation.dart';
 import 'package:findme/pages/Location/LiveLocationUpdates.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:localization/localization.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,15 +24,45 @@ void main() async {
   }
   await FirebaseMessaging.instance.getInitialMessage();
 
-  runApp(const MyApp());
+  // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+
+  runApp(const MyApp(isNotification: false));
+}
+
+void onDidReceiveNotificationResponse(
+    NotificationResponse notificationResponse) async {
+  final String? payload = notificationResponse.payload;
+  if (notificationResponse.payload != null) {
+    debugPrint('notification payload: $payload');
+  }
+  var context;
+  runApp(const MyApp(isNotification: true));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool? isNotification;
+
+  const MyApp({Key? key, required this.isNotification}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     LocalJsonLocalization.delegate.directories = ['lib/i18n'];
+    if (isNotification == true) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LocationConfirmationPage()),
+        );
+      });
+    }
     return MaterialApp(
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
